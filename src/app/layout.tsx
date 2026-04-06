434a77c834e10da56cdc98fb4fccc5b044000c96
iljoung oh <70251881+happy8131@users.noreply.github.com>
2026-04-06 21:55:46 +0900

    하이드세이션  추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
+import { ThemeProvider } from '@/components/providers/theme-provider'
+import { Toaster } from '@/components/ui/sonner'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import { ThemeProvider } from '@/components/providers/theme-provider'
-import { Toaster } from '@/components/ui/sonner'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
